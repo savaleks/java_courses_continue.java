0aa75c21cf6cb430cab96fbfc73590db07c54b6f
savaleks <savaleks@gmail.com>
2018-07-24 18:57:22 +0300

    if else challenge in java masterclass

---
+++ src/methods/Challenges.java
@@ -0,0 +1,81 @@
+package methods;
+
+public class Challenges {
+
+    public static void main(String[] args) {
+//        System.out.println( area(5.0));
+//        System.out.println(area(5.0, 4.0));
+//        System.out.println(area(-1));
+//        System.out.println(area(-1.0, 4.0));
+//        System.out.println(area(1.0, -5.0));
+//        printYearsAndDays(525600);
+//        printYearsAndDays(1051200);
+//        printYearsAndDays(561600);
+//        printYearsAndDays(-525600);
+
+//        printEqual(1,1,1);
+//        printEqual(1,1,2);
+//        printEqual(-1,-1,-1);
+//        printEqual(1, 2,3);
+
+//        System.out.println(isCatPlaying(true, 10));
+//        System.out.println(isCatPlaying(false, 36));
+//        System.out.println(isCatPlaying(false, 35));
+//        System.out.println(isCatPlaying(true, 45));
+//
+//    }
+
+//    public static double area(double radius){
+//        final double RADIUS =3.14159;
+//        if (radius < 0) {
+//            return -1;
+//        }
+//        double circleArea = (radius*radius)*RADIUS;
+//        return circleArea;
+//    }
+//
+//    public static double area(double x, double y){
+//        if (x < 0 || y < 0){
+//            return -1;
+//        }
+//        double rectangleArea = x*y;
+//        return rectangleArea;
+//    }
+
+//    public static void printYearsAndDays(long minutes) {
+//        if (minutes < 0) {
+//            System.out.println("Invalid Value");
+//        } else {
+//            long hours = minutes / 60;
+//            long days = hours / 24;
+//            int years = (int) (days / 365);
+//            int leftDays = (int) ((days) - (years * 365));
+//            System.out.println(minutes + "min = " + years + "y and " + leftDays + "d");
+//        }
+//    }
+
+    //    public static void printEqual(int x, int y, int z){
+//        if ((x < 0 || y < 0 || z < 0)){
+//            System.out.println("Invalid Value");
+//        }
+//        else if (x == y && x == z && y == z){
+//            System.out.println("All numbers are equal");
+//        }
+//        else if (x != y && x != z && y != z) {
+//            System.out.println("All numbers are different");
+//        }
+//        else {
+//            System.out.println("Neither all are equal or different");
+//        }
+//    }
+//    public static boolean isCatPlaying(boolean summer, int temperature) {
+//        if (summer == true && (temperature >= 25 && temperature <= 45)
+//                || summer == false && (temperature >= 25 && temperature <= 35)) {
+//
+//            return true;
+//        }
+//
+//        return false;
+  }
+}
+
