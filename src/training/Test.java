0f8776195a9ea939e8c5f74e807ceb3e587476dd
savaleks <savaleks@gmail.com>
2018-08-10 11:38:34 +0300

    output id and age;

--- src/training/Test.java
+++ src/training/Test.java
@@ -1,12 +1,33 @@
 package training;
 
+import lesson21.moUserInput;
+
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
-
-        //whois.arin.net
-        System.out.println(WhoisRIR.ARIN.url());
-
+        Scanner keyboard = new Scanner(System.in);
+        moUserInput mValidator = new moUserInput();
+        int ssn = mValidator.askInputInt("Please type your ID number: ");
+        double age =  mValidator.askInputDouble("Please type your age: ");
+        System.out.println("Accepted Results:");
+        System.out.println("ID #: " + ssn);
+        System.out.println("Age: " + age);
     }
-
+    public Boolean isType(String testStr, String type) {
+        try {
+            if (type.equalsIgnoreCase("float")) {
+                Float.parseFloat(testStr);
+            } else if (type.equalsIgnoreCase("int")) {
+                Integer.parseInt(testStr);
+            } else if (type.equalsIgnoreCase("double")) {
+                Double.parseDouble(testStr);
+            }
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
 }
+
